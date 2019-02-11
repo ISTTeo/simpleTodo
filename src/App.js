ff2ef09e071a385e4914946bbf0fd7c57f23b23f
Teo <ldptek@gmail.com>
2019-02-11 23:13:58 +0000

    Working basic todo app

--- src/App.js
+++ src/App.js
@@ -3,43 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 import todosData from "./todosData"
 import TodoItem from "./TodoItem"
+import TodoForm from './TodoForm'
 class App extends Component {
-	
+
 	constructor() {
 		super()
 		this.state = {
 			todos: todosData
 		}
     this.handleChange = this.handleChange.bind(this)
+		this.addTodo = this.addTodo.bind(this)
   }
-  
+
   handleChange(id) {
-    this.setState(prevState => {
-      const updatedTodos = this.state.todos.map(todo => {
-        if(todo.id === id) {
-          todo.completed = !todo.completed
-        }
-        return todo
-      })
-      return {
-        todos:updatedTodos
-      }
-    })
+		const allTodos = this.state.todos
+		const filtered = allTodos.filter(x => x.id !== id)
+		this.setState({todos:filtered})
+
   }
+
+	addTodo(value,todos) {
+		const allTodos = this.state.todos
+		const lastTodo = this.state.todos.slice(-1)[0]
+		const newId = lastTodo.id + 1
+		const newTodo = {id: newId, text: value, completed:false}
+		allTodos.push(newTodo)
+		this.setState({todos:allTodos})
+	}
+
   render() {
-    const todoItems = this.state.todos.map( item => 
-      <TodoItem 
-        key={item.id} 
-        item={item} 
+    const todoItems = this.state.todos.map( item =>
+      <TodoItem
+        key={item.id}
+        item={item}
         handleChange={this.handleChange}
       />
     )
-
+		const text = ""
     return (
       <div className="todo-list">
         {todoItems}
+				<TodoForm addTodo={this.addTodo} todos={this.state.todos}/>
       </div>
-    );
+		);
   }
 }
 
---
+++ src/TodoForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(event) {
+    event.preventDefault() //meter depois
+
+    this.props.addTodo(this.input.value,this.props.todos)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          New todo:
+        </label>
+          <input type="text" ref={(input) => this.input = input}/>
+
+      </form>
+    )
+  }
+}
+
+export default TodoForm
