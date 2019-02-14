22d492908cb0d7564d730d4b2ee2ad9050f755da
Teo <ldptek@gmail.com>
2019-02-14 16:04:22 +0000

    Allow for todos to be completed, to be individually deleted and delete all completed todos.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,11 +3,12 @@ import React from 'react'
 function TodoItem(props) {
     return (
         <div>
-            <input 
-                type="checkbox" 
-                checked={props.item.completed} 
-                onChange={() => props.handleChange(props.item.id)}
+            <input
+                type="checkbox"
+                checked={props.item.completed}
+                onChange={() => props.checkIt(props.item.id)}
             />
+            <button onClick={() => props.delTodo(props.item.id)}>X</button>
             <p>
                 {props.item.text}
             </p>
